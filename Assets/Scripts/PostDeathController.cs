558ccab462cd46c9b96241572563242b704e73a2
tucoff <baracho.arturraffael@gmail.com>
2022-10-10 23:34:51 -0300

    last fixes

--- Assets/Scripts/PostDeathController.cs
+++ Assets/Scripts/PostDeathController.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class PostDeathController : MonoBehaviour
 {
+    public void Update() 
+    {
+        transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>().text = 
+        GameObject.FindGameObjectWithTag("Score").GetComponent<TextMeshProUGUI>().text;
+    }
+
     public void PlayAgain()
     {
         SceneManager.LoadScene("SampleScene");
